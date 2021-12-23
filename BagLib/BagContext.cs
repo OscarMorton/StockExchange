34727c14481d53f6f4bf0ac0b0b412ccfd43207d
Pablo Mocholi <pbmocholi_98@hotmail.es>
2021-12-23 13:07:00 +0100

    Base and Country Management Implemented. closes #1

---
+++ BagLib/BagContext.cs
@@ -0,0 +1,45 @@
+﻿using BagLib.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace BagLib
+{
+    public partial class BagContext : DbContext
+    {
+        private readonly string LOG = "D:\\Capgemini\\Bootcamp\\CURSO_NET_AVANZADO\\BagMVC\\LOG.txt";
+        private readonly StreamWriter _logStream;
+
+        public DbSet<Country> Country { get; set; }
+
+        public DbSet<Market> Market { get; set; }
+
+        public DbSet<Stock> Stock { get; set; }
+
+        public DbSet<Currency> Currency { get; set; }
+
+        public DbSet<BagUser> BagUser { get; set; }
+
+        public DbSet<MyStock> MyStock { get; set; }
+
+        public BagContext(DbContextOptions<BagContext> options) : base(options)
+        {
+            
+            _logStream = new StreamWriter(LOG, append: true);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .LogTo(_logStream.WriteLine)
+                    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BagContextFinal;Trusted_Connection=True;");
+            }
+        }
+    }
+}
---
+++ BagLib/Migrations/20211220222317_01_Basic_Final.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BagLib.Migrations
+{
+    public partial class _01_Basic_Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
